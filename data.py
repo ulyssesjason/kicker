2bdf13af1b3692437a46b4a73fbc3b34924b2d8b
ulyssesjason <ulyssesjason@gmail.com>
2013-02-19 14:50:53 -0600

    initial first

--- data.py
+++ data.py
@@ -1,6 +1,42 @@
+class base():
+	def __init__(self,email=None):
+		self.email=email
 
-class basic_contact:
-	email=""
-	first_name=""
-	last_name=""
-	
+class basic_contact(base):
+    def __init__(self, email=None, first_name=None, last_name=None):
+    	self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+class contact_work(base):
+	def __init__(self, email=None, job=None, company=None, work_phone=None):
+		self.email = email
+		self.job = job
+		self.company = company
+		self.work_phone = work_phone
+
+class contact_family(base):
+	def __init__(self, email = None, home_phone=None, address1 = None,
+				 address2 = None, address3 = None, city = None, state = None,
+				 other_state=None, country = None, zipcode = None, subzipcode = None ):
+		self.email = email
+		self.home_phone = home_phone
+		self.address1 = address1
+		self.address2 = address2
+		self.address3 = address3
+		self.city = city
+		self.state = state
+		self.other_state = other_state
+		self.country = country
+		self.zipcode = zipcode
+
+class contact_note(base):
+	def __init__(self, email = None, note = None):
+		self.email = email
+		self.note = note
+
+		
+        
+
+
+		
--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import sys
 import csv
+import data 
+import sqlite3 as lite
 
+def 
 
 
 
 def main():
 	with open("export.csv","rb") as f:
 		reader=csv.reader(f)
-	
+		
 		for email,fname,mname,lname,title,company,wphone,hphone,address1,\
-			address2,address3,city,state,other_state,country,zipcode,subzipcode,notes in reader:
+			address2,address3,city,state,other_state,country,zipcode,sub_zipcode,notes in reader:
 			
 
 
